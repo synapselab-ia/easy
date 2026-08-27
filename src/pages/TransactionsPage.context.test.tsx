95be7dac0bc5db87c21fc45ac6fb0303084d70ae
synapselab-ia <synapselab.ia@gmail.com>
2026-08-27 10:55:17 -0300

    test(transactions): target reseller searchable select by label

--- src/pages/TransactionsPage.context.test.tsx
+++ src/pages/TransactionsPage.context.test.tsx
@@ -82,7 +82,7 @@ describe('TransactionsPage reseller context', () => {
         renderPage('/transactions?resellerId=1');
 
         await waitFor(() => expect(screen.getByText('Mariazinha')).toBeInTheDocument());
-        const resellerSelect = screen.getByTestId('mock-searchable-select');
+        const resellerSelect = screen.getByLabelText(/^Revendedor$/i);
         expect((resellerSelect as HTMLSelectElement).value).toBe('1');
     });
 
@@ -90,7 +90,7 @@ describe('TransactionsPage reseller context', () => {
         renderPage('/transactions?resellerId=not-a-number');
 
         await waitFor(() => expect(screen.getByText('Mariazinha')).toBeInTheDocument());
-        const resellerSelect = screen.getByTestId('mock-searchable-select');
+        const resellerSelect = screen.getByLabelText(/^Revendedor$/i);
         expect((resellerSelect as HTMLSelectElement).value).toBe('');
     });
 });
